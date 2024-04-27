d439e281d25f230b07e82ebe2f8937a72c32737d
erhaemael <rahma.alia.tif421@polban.ac.id>
2024-04-27 11:03:14 +0700

    validasi

--- frontend/catatan-belajar/src/components/custom/FormCatatan.tsx
+++ frontend/catatan-belajar/src/components/custom/FormCatatan.tsx
@@ -43,6 +43,9 @@ const FormCatatan: React.FC<FormCatatanProps> = ({
   useEffect(() => {
     setIsPrivasi(privasi === Privasi.PUBLIC);
   }, [privasi]);
+
+  const [isJudulValid, setIsJudulValid] = useState<boolean>(true);
+  const [isIsiValid, setIsIsiValid] = useState<boolean>(true);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleIsiChange = (value: string) => {
@@ -71,20 +74,51 @@ const FormCatatan: React.FC<FormCatatanProps> = ({
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
+
+      console.log("Judul Catatan:", judul_catatan);
+      console.log("Isi Catatan:", isi_catatan);
+
+      if (!judul_catatan.trim()) {
+        setIsJudulValid(false);
+      } else {
+        setIsJudulValid(true);
+      }
+
+      const isContentEmpty = !!(isi_catatan.trim() === '<p><br></p>');
+      if (!isi_catatan.trim() || isContentEmpty) {
+        setIsIsiValid(false);
+      } else {
+        setIsIsiValid(true);
+      }
+
+      if (!judul_catatan.trim() && (!isi_catatan.trim() || isContentEmpty)) {
+        setIsJudulValid(false);
+        setIsIsiValid(false);
+        return;
+      }
+
+      if (!judul_catatan.trim()) {
+        return;
+      }
+
+      if (!isi_catatan.trim() || isContentEmpty) {
+        return;
+      }
+    
       let data: any;
       setIsLoading(true);
-      
-      if (method != "DELETE") {
+    
+      if (method !== "DELETE") {
         data = {
           id_akun: 2,
           judul_catatan: judul_catatan,
           isi_catatan: isi_catatan,
-          privasi: isPrivasi ? Privasi.PUBLIC : Privasi.PRIVATE,
+          privasi: privasi,
           gambar: gambar,
           nama_tag: nama_tag,
         };
       }
-
+    
       const response = await fetch(
         `http://localhost:3030/api/catatanbelajar/${
           id_catatan ? id_catatan : ""
@@ -97,13 +131,13 @@ const FormCatatan: React.FC<FormCatatanProps> = ({
           body: JSON.stringify(data),
         }
       );
-
+    
       if (!response.ok) {
         throw new Error("Failed to create or update catatan");
       }
-
+    
       await new Promise((resolve) => setTimeout(resolve, 1000));
-
+    
       setIsLoading(false);
       onSubmit(true);
     } catch (error) {
@@ -111,57 +145,73 @@ const FormCatatan: React.FC<FormCatatanProps> = ({
       alert("Failed to create or update catatan. Please try again.");
     }
   };
+  
+  
 
   return (
     <form onSubmit={handleSubmit} className="mt-4">
       <div className="flex flex-col space-y-4">
-        <Input
-          type="text"
-          placeholder="Judul Catatan"
-          onChange={(e) =>
-            onCatatanDataChange({ judul_catatan: e.target.value })
-          }
-          value={judul_catatan}
-        />
-
-        <ReactQuill
-          value={isi_catatan}
-          onChange={handleIsiChange}
-          placeholder="Buat Catatan..."
-          id="form_isi"
-          theme="snow"
-          modules={{
-            toolbar: [
-              ["image"],
-              [{ font: [] }],
-              [{ header: [1, 2, 3, false] }],
-              ["bold", "italic", "underline"],
-              [{ list: "ordered" }, { list: "bullet" }],
-              [{ align: [] }],
-              [{ color: [] }],
-              ["clean"],
-            ],
-          }}
-          formats={[
-            "font",
-            "image",
-            "header",
-            "bold",
-            "italic",
-            "underline",
-            "list",
-            "bullet",
-            "align",
-            "color",
-          ]}
-        />
-        <label
-          htmlFor="form_isi"
-          className="block text-sm text-gray-400 mt-1 ml-1 text-left"
-          style={{ fontSize: "12px" }}
-        >
-          Total ukuran gambar pada isi tidak boleh lebih dari 100KB
-        </label>
+        <div className="text-left">
+          <Input
+            type="text"
+            placeholder="Judul Catatan"
+            onChange={(e) =>
+              onCatatanDataChange({ judul_catatan: e.target.value })
+            }
+            value={judul_catatan}
+            className={!isJudulValid ? "border border-red-500" : ""}
+          />
+          {!isJudulValid && (
+            <label className="text-sm text-red-600 dark:text-red-500">
+              Judul Catatan is required
+            </label>
+          )}
+        </div>
+        <div className="text-left">
+          <ReactQuill
+            value={isi_catatan}
+            onChange={handleIsiChange}
+            placeholder="Buat Catatan..."
+            id="form_isi"
+            theme="snow"
+            modules={{
+              toolbar: [
+                ["image"],
+                [{ font: [] }],
+                [{ header: [1, 2, 3, false] }],
+                ["bold", "italic", "underline"],
+                [{ list: "ordered" }, { list: "bullet" }],
+                [{ align: [] }],
+                [{ color: [] }],
+                ["clean"],
+              ],
+            }}
+            formats={[
+              "font",
+              "image",
+              "header",
+              "bold",
+              "italic",
+              "underline",
+              "list",
+              "bullet",
+              "align",
+              "color",
+            ]}
+          />
+          {!isIsiValid && (
+            <label className="text-sm text-red-600 dark:text-red-500">
+              Isi Catatan is required
+            </label>
+          )}
+          <label
+            htmlFor="form_isi"
+            className="block text-sm text-gray-400 mt-1 ml-1 text-left"
+            style={{ fontSize: "12px" }}
+          >
+            Total ukuran gambar pada isi tidak boleh lebih dari 100KB
+          </label>
+        </div>
 
         <div className="text-left">
           <Label htmlFor="tag">Tag</Label>
@@ -173,7 +223,7 @@ const FormCatatan: React.FC<FormCatatanProps> = ({
             id="tag"
           />
           <label
-            htmlFor="tags"
+            htmlFor="tag"
             className="block text-sm text-gray-400 mt-1 ml-1 text-left"
             style={{ fontSize: "12px" }}
           >
@@ -231,21 +281,33 @@ const FormCatatan: React.FC<FormCatatanProps> = ({
 
             <Button
               type="submit"
-              className={`bg-[#38B0AB] hover:bg-[#22918D] text-white ml-2 ${
-                !isi_catatan.trim() ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-              disabled={isLoading || !isi_catatan.trim()}
+              className={`bg-[#38B0AB] hover:bg-[#22918D] text-white ml-2 `}
+              disabled={isLoading}
             >
-            {isLoading ? (
+              {isLoading ? (
                 <div className="flex items-center">
-                    <svg aria-hidden="true" className="inline w-5 h-5 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                    </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="inline w-5 h-5 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
                   {method === "POST" ? "Simpan" : "Update"}
                 </div>
+              ) : method === "POST" ? (
+                "Simpan"
               ) : (
-                method === "POST" ? "Simpan" : "Update"
+                "Update"
               )}
             </Button>
           </div>
